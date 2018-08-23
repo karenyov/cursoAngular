fff87d21e0e2e7a5f2bb160b61ec7991aa2c5161
Karen Vicente <karenyasmin37@gmail.com>
2018-08-23 18:35:41 -0300

    diretiva ngfor

---
+++ diretivas/src/app/ng-for/ng-for.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for',
+  templateUrl: './ng-for.component.html',
+  styleUrls: ['./ng-for.component.css']
+})
+export class NgForComponent implements OnInit {
+
+  cursos: string[] = ['Angular 2', 'Java'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
